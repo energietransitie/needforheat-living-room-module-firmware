2316283ce6390500affa33eb17c183f66251bb2b
Maarten Vermeulen <maartenvermeulen70@gmail.com>
2021-04-15 13:07:10 +0200

    I2C interrupt commented out
    * The ISR is not configured properly and would generate an error,
    it is temporarily commented out

--- co2_sensor/src/i2c.c
+++ co2_sensor/src/i2c.c
@@ -50,9 +50,9 @@ void i2c_init(void)
     ESP_ERROR_CHECK(i2c_driver_install(I2C_PORT, conf.mode, 0, 0, ESP_INTR_FLAG_LEVEL3)); 
 
     // configure ISR (could not be tested properly, yet)
-    intr_handle_t handle;
-    esp_intr_alloc_intrstatus(ETS_I2C_EXT0_INTR_SOURCE, ESP_INTR_FLAG_LEVEL3, I2C_STATUS_REGISTER, I2C_STATUS_TIMEOUT, i2c_isr, NULL, &handle);
-    i2c_isr_register(I2C_PORT, i2c_isr, NULL, ESP_INTR_FLAG_LEVEL3, &handle);
+    //intr_handle_t handle;
+    //ESP_ERROR_CHECK(esp_intr_alloc_intrstatus(ETS_I2C_EXT0_INTR_SOURCE, ESP_INTR_FLAG_LEVEL3, I2C_STATUS_REGISTER, I2C_STATUS_TIMEOUT, i2c_isr, NULL, &handle));
+    //ESP_ERROR_CHECK(i2c_isr_register(I2C_PORT, i2c_isr, NULL, ESP_INTR_FLAG_LEVEL3, &handle));
     
     // set a max. time out for 13ms
     i2c_set_timeout(I2C_PORT, TIMEOUT_VAL);
