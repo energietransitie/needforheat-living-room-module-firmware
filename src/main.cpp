acb4f05a22577dac3884725a79bb9027aa2e30bd
n-vr <43145188+n-vr@users.noreply.github.com>
2022-08-16 09:16:02 +0000

    Add MAC-address for presence detection

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <generic_tasks.hpp>
 #include <scheduler.hpp>
 #include <scd41_task.hpp>
+#include <presence_detection.hpp>
 
 constexpr const char *DEVICE_TYPE_NAME = "CO2-meter-SCD4x";
 
@@ -12,6 +13,9 @@ extern "C" void app_main(void)
 	GenericTasks::AddTasksToScheduler();
 
 #ifdef M5STACK_COREINK
+	// This is a fake MAC-address.
+	PresenceDetection::AddMacAddress("ab:ff:0c:fc:ba:5c");
+
 	// Add the SCD41 task to the scheduler.
 	Scheduler::AddTask(SCD41Task, "SCD41 task", 4096, nullptr, 1, Scheduler::Interval::MINUTES_10);
 #endif // M5STACK_COREINK
